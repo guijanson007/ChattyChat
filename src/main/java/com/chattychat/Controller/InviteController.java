58d7c1a76f483f1491c86656634de5ebb9b32fde
guijanson007 <guilhermejanson@gmail.com>
2026-08-20 19:51:02 -0300

    invite controller

---
+++ src/main/java/com/chattychat/Controller/InviteController.java
@@ -0,0 +1,9 @@
+package com.chattychat.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/rooms/invites")
+public class InviteController {
+}
